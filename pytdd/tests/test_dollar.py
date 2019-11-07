3b1ca589b12dc9772233e599b074dcd52c14e66f
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-11-07 01:30:10 +0000

    3.1 Tests for dollar.equal

--- pytdd/tests/test_dollar.py
+++ pytdd/tests/test_dollar.py
@@ -3,8 +3,13 @@ from pytdd.dollar import Dollar
 
 def test_dollar_add():
     num = Dollar(10)
-    assert num.add(2) == Dollar(12)
+    assert num.add(2).get_value() == Dollar(12).get_value()
 
 
 def test_get_value():
     assert Dollar(5).get_value() == 5
+
+
+def test_equal():
+    assert Dollar(1).equal(Dollar(1))
+    assert not Dollar(1).equal(Dollar(2))
