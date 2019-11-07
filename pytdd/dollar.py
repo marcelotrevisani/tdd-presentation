804d6de96c9b857afabb349317a2b53b21fe1020
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-11-07 13:37:44 +0000

    1.3 Refactoring Dollar.conver_to_pound

--- pytdd/dollar.py
+++ pytdd/dollar.py
@@ -1,3 +1,9 @@
+import requests
+
+URL_API = r"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE"
+API_KEY = "FJOSXYTTSW4VUT7H"
+
+
 class Dollar:
     def __init__(self, value):
         self._value = value
@@ -13,4 +19,12 @@ class Dollar:
         return self._value == compare.value
 
     def convert_to_pound(self):
-        return Dollar(7.77)
+        url_request = (
+            f"{URL_API}" f"&from_currency=USD&to_currency=GBP&apikey={API_KEY}"
+        )
+
+        response = requests.get(url_request)
+        data_exchange = response.json()
+        exchange_rate = data_exchange["Realtime Currency Exchange Rate"]
+        exchange_rate = float(exchange_rate["5. Exchange Rate"])
+        return Dollar(self.value * exchange_rate)
