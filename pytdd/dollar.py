6ddacc71144a369ffe7c119fb1bdf46ea87dda4f
Marcelo Duarte Trevisani <marceloduartetrevisani@gmail.com>
2019-11-07 01:43:57 +0000

    5.2 Implementation Dollar.__eq__

--- pytdd/dollar.py
+++ pytdd/dollar.py
@@ -8,5 +8,5 @@ class Dollar():
     def get_value(self):
         return self._value
 
-    def equal(self, compare):
+    def __eq__(self, compare):
         return self._value == compare.get_value()
